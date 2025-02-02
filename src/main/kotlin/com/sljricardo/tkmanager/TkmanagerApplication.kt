0fb4d4d04620c43bf78436bc986ba0eb6e59d189
sljricardo <sljricardo@gmail.com>
2025-02-02 10:31:02 +0000

    init: spring boot app

---
+++ src/main/kotlin/com/sljricardo/tkmanager/TkmanagerApplication.kt
@@ -0,0 +1,11 @@
+package com.sljricardo.tkmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TkmanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TkmanagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/sljricardo/tkmanager/TkmanagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sljricardo.tkmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TkmanagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
