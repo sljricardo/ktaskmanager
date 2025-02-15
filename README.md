3e34b2e307b13ae2e0c69a6e2ba99f31bab36aaf
sljricardo <sljricardo@gmail.com>
2025-02-15 12:44:11 +0000

    feat: add jooq

---
+++ README.md
@@ -0,0 +1,12 @@
+## Ktaskmanager
+
+### Description
+Basic CRUD API to manage tasks
+
+### Requirements
+ - sqlite
+
+### Development
+1. create the tables from 'src/main/resources/db/migration' 
+2. run jooq generator ./gradlew generateJooq 
+3. run the app
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.jooq.meta.jaxb.Logging
+
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("nu.studer.jooq") version "9.0"
 }
 
 group = "com.sljricardo"
@@ -30,6 +33,14 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("org.jooq:jooq:3.18.8")
+	implementation("org.jooq:jooq-meta:3.18.8")
+	implementation("org.jooq:jooq-codegen:3.18.8")
+
+	jooqGenerator("org.jooq:jooq-meta:3.18.8")
+	jooqGenerator("org.jooq:jooq-codegen:3.18.8")
+	jooqGenerator("org.xerial:sqlite-jdbc:3.45.3.0") // Ensure latest SQLite JDBC version
 }
 
 kotlin {
@@ -40,4 +51,38 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jooq {
+	version.set("3.18.8")
+
+	configurations {
+		create("main") {
+			jooqConfiguration.apply {
+				logging = Logging.WARN
+				jdbc.apply {
+					driver = "org.sqlite.JDBC"
+					url = "jdbc:sqlite:./mydb.db"
+				}
+				generator.apply {
+					name = "org.jooq.codegen.KotlinGenerator"
+					database.apply {
+						name = "org.jooq.meta.sqlite.SQLiteDatabase"
+						//inputSchema = "main"
+					}
+					generate.apply {
+						isDeprecated = false
+						isRecords = true
+						isImmutablePojos = true
+						isFluentSetters = true
+					}
+					target.apply {
+						packageName = "com.sljricardo.jooq"
+						directory = "src/main/kotlin/generator"
+					}
+					strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+				}
+			}
+		}
+	}
 }
