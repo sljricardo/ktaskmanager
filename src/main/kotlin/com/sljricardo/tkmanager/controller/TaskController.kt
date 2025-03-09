ee0a5d3d13e5e52db822199667b17bea47daeafe
sljricardo <sljricardo@gmail.com>
2025-03-09 09:34:27 +0000

    chore: remove unused imports

--- src/main/kotlin/com/sljricardo/tkmanager/controller/TaskController.kt
+++ src/main/kotlin/com/sljricardo/tkmanager/controller/TaskController.kt
@@ -5,17 +5,12 @@ import com.sljricardo.tkmanager.dto.ChangeTaskStateRequest
 import com.sljricardo.tkmanager.model.Task
 import com.sljricardo.tkmanager.dto.NewTaskRequest
 import com.sljricardo.tkmanager.service.TaskService
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.web.ErrorResponse
-import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
